54a8bbe0f5f63d94c6f745afd4aa5dc31233f53a
Daniel Martí <mvdan@mvdan.cc>
2015-12-09 10:04:16 +0100

    Split test code in preparation for more tests

--- main_test.go
+++ main_test.go
@@ -7,43 +7,43 @@ import (
 	"bytes"
 	"go/token"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"testing"
 )
 
-func TestAll(t *testing.T) {
+func doTest(t *testing.T, inPaths []string, outPath string) {
+	p := &goPkg{
+		fset: token.NewFileSet(),
+	}
+	for _, inPath := range inPaths {
+		if err := p.parsePath(inPath); err != nil {
+			t.Fatal(err)
+		}
+	}
+	var b bytes.Buffer
+	if err := p.check(&b); err != nil {
+		t.Fatal(err)
+	}
+	expBytes, err := ioutil.ReadFile(outPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	exp := string(expBytes)
+	got := b.String()
+	if exp != got {
+		t.Fatalf("Mismatch in %s.\nExpected:\n%sGot:\n%s",
+			outPath, exp, got)
+	}
+}
+
+func TestSingle(t *testing.T) {
 	testsGlob := filepath.Join("testdata", "*.go")
 	matches, err := filepath.Glob(testsGlob)
 	if err != nil {
 		t.Fatal(err)
 	}
 	for _, inPath := range matches {
-		f, err := os.Open(inPath)
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer f.Close()
 		outPath := inPath + ".out"
-		expBytes, err := ioutil.ReadFile(outPath)
-		if err != nil {
-			t.Fatal(err)
-		}
-		p := &goPkg{
-			fset: token.NewFileSet(),
-		}
-		if err := p.parsePath(inPath); err != nil {
-			t.Fatal(err)
-		}
-		var b bytes.Buffer
-		if err := p.check(&b); err != nil {
-			t.Fatal(err)
-		}
-		exp := string(expBytes)
-		got := b.String()
-		if exp != got {
-			t.Fatalf("Mismatch in %s.\nExpected:\n%sGot:\n%s",
-				inPath, exp, got)
-		}
+		doTest(t, []string{inPath}, outPath)
 	}
 }
