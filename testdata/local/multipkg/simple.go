87f3716b753deeab6f18d9de744ed58f7d10fa3b
Daniel Martí <mvdan@mvdan.cc>
2017-03-28 14:47:06 +0100

    Simplify multipkg test
    Since we no longer sort the input packages, this test output is
    non-deterministic.

--- testdata/local/multipkg/simple.go
+++ testdata/local/multipkg/simple.go
@@ -1,14 +1,1 @@
 package multipkg
-
-type Closer interface {
-	Close()
-}
-
-type ReadCloser interface {
-	Closer
-	Read()
-}
-
-func BasicWrong(rc ReadCloser) { // WARN rc can be Closer
-	rc.Close()
-}
